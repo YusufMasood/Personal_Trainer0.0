d3624a803e500143dde00195ae4a1720516526fc
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-01-27 20:43:01 +0530

    A fresh Start

--- app/src/main/java/com/example/myapplication/Ui/Intro Video.kt
+++ app/src/main/java/com/example/myapplication/Ui/Intro Video.kt
@@ -1,12 +1,10 @@
 package com.example.myapplication.Ui
 
-import android.text.Layout
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
---
+++ app/src/main/java/com/example/myapplication/Ui/Loggin Page.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.Ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LogginPage(){
+
+
+}
+
+
+@Preview
+@Composable
+fun show1(){
+
+}
