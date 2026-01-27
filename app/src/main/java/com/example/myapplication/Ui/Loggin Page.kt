7a2e8669b5b55def1328a882029b940f2cbce718
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-01-27 20:49:57 +0530

    Adding Loggin Page

--- app/src/main/java/com/example/myapplication/Ui/Loggin Page.kt
+++ app/src/main/java/com/example/myapplication/Ui/Loggin Page.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication.Ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun LogginPage(){
 
+    Box(){
+
+    }
 
 }
 
