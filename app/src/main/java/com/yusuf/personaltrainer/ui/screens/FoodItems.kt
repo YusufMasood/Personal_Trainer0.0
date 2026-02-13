91c4361f695ad704364618e8e946c4e0fcd03c2f
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-02-13 18:15:01 +0530

    Configuring the data set of food items

--- app/src/main/java/com/yusuf/personaltrainer/ui/screens/FoodItems.kt
+++ app/src/main/java/com/yusuf/personaltrainer/ui/screens/FoodItems.kt
@@ -1,8 +1,14 @@
 package com.yusuf.personaltrainer.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun FoodItems(){
+
+    Box(){
+        Text("We will show our food data here")
+    }
 
 }
