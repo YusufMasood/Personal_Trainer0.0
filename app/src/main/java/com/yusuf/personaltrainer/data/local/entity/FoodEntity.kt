5d3217ef7f42b53208be37cbd52df06ab5e2fa08
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-02-14 00:15:24 +0530

    Added the Entity for the food items

---
+++ app/src/main/java/com/yusuf/personaltrainer/data/local/entity/FoodEntity.kt
@@ -0,0 +1,22 @@
+package com.yusuf.personaltrainer.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "foods",
+    indices = [Index(value = ["name"])]
+)
+data class FoodEntity(
+    @PrimaryKey
+    val foodId: Long,
+
+    val name: String,
+
+    val caloriesPer100g: Double,
+    val proteinPer100g: Double,
+    val carbsPer100g: Double,
+    val fatPer100g: Double,
+    val fiberPer100g: Double
+)
