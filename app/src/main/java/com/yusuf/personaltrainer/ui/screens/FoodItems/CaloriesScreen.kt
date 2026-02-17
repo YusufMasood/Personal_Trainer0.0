421d29447bea92e816465df58c756d458198e547
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-02-17 18:07:56 +0530

    Creating progress bar in meals screen

--- app/src/main/java/com/yusuf/personaltrainer/ui/screens/FoodItems/CaloriesScreen.kt
+++ app/src/main/java/com/yusuf/personaltrainer/ui/screens/FoodItems/CaloriesScreen.kt
@@ -1,12 +1,14 @@
 package com.yusuf.personaltrainer.ui.screens.FoodItems
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yusuf.personaltrainer.ui.components.AddFoodCard
+import com.yusuf.personaltrainer.ui.components.CaloriesCounterCard
 
 
 @Composable
@@ -14,9 +16,23 @@ fun CaloriesScreen(){
 
     Box(modifier = Modifier.padding(12.dp)){
 
+        Column() {
 
-        // cards for add food items
-        AddFoodCard()
+            //Caloties Progress
+            CaloriesCounterCard(
+                1600,
+                2000,
+                170,
+                256,
+                60,
+                {}
+            )
+
+
+            // cards for add food items
+            AddFoodCard()
+
+        }
     }
 
 }
